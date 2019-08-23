cef18ca3aebd1bc04b1124e527633615b4aa509e
Joselyne97 <joselynejojo740@gmail.com>
2019-08-23 20:47:33 +0200

    I added quote form button

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
   public showDescription: boolean;
-  constructor(public id: number, public quotation: string, public author: string,public posted: string, public completeDate: Date){
+  constructor(public id: number, public quotation: string, public author: string, public posted: string, public completeDate: Date){
     this.showDescription=false;
   }
 }
