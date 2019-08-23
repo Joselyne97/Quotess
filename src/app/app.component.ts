2fe286c8a6db56b6733f39f3f1f335a696dd908d
Joselyne97 <joselynejojo740@gmail.com>
2019-08-23 14:45:47 +0200

    I added the quotes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-
+import { Quote } from './quote';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  quote = 'Watch Finding Nemo'
+  Quotes:Quote[] = [
+   {id:1,name:'There is nothing permanent except change'},
+   {id:2,name:'Let us sacrifice ourselfs today so that our children can have a better tomorrow'},
+   {id:3,name:'There is no charm equal to tenderness of heart'},
+   {id:4,name:'All that we see or seem is but a dream within a dream'},
+   {id:5,name:'The only journey is the one within'},
+   {id:6,name:'Think in the morning, act in the noon, eat in the evening and sleep in the night'},
+    ];
 }
