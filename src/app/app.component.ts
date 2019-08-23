dad3786655f5ab940af7d39b88ab00173e3fe0f4
Joselyne97 <joselynejojo740@gmail.com>
2019-08-23 15:50:05 +0200

    I edited the quote's html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,12 @@ import { Quote } from './quote';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  Quotes:Quote[] = [
-   {id:1,name:'There is nothing permanent except change'},
-   {id:2,name:'Let us sacrifice ourselfs today so that our children can have a better tomorrow'},
-   {id:3,name:'There is no charm equal to tenderness of heart'},
-   {id:4,name:'All that we see or seem is but a dream within a dream'},
-   {id:5,name:'The only journey is the one within'},
-   {id:6,name:'Think in the morning, act in the noon, eat in the evening and sleep in the night'},
+    Quotes:Quote[] = [
+      {id:1,name:'There is nothing permanent except change',description:'Heraclitus'},
+      {id:2,name:'Let us sacrifice ourselfs today so that our children can have a better tomorrow',description:'A.P.J.Abdul Kalam'},
+      {id:3,name:'There is no charm equal to tenderness of heart',description:'Jane Austen'},
+      {id:4,name:'All that we see or seem is but a dream within a dream',description:'Edgar Allan Poe'},
+      {id:5,name:'The only journey is the one within',description:'Rainer Maria Rilke'},
+      {id:6,name:'Think in the morning, act in the noon, eat in the evening and sleep in the night',description:'Will Rogers'},
     ];
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,12 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Quote } from '../quote';
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
-
+  quotes:string[];
+  Quotes:Quote[] = [
+     {id:1,name:'There is nothing permanent except change',description:'Heraclitus'},
+     {id:2,name:'Let us sacrifice ourselfs today so that our children can have a better tomorrow',description:'A.P.J.Abdul Kalam'},
+     {id:3,name:'There is no charm equal to tenderness of heart',description:'Jane Austen'},
+     {id:4,name:'All that we see or seem is but a dream within a dream',description:'Edgar Allan Poe'},
+     {id:5,name:'The only journey is the one within',description:'Rainer Maria Rilke'},
+     {id:6,name:'Think in the morning, act in the noon, eat in the evening and sleep in the night',description:'Will Rogers'},
+   ];
   constructor() { }
 
   ngOnInit() {
