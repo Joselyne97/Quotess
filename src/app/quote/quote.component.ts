0ebd1c7f5e09d7becde7fd79d95fd61f8cf0d99c
Joselyne97 <joselynejojo740@gmail.com>
2019-08-23 18:05:40 +0200

    I added the details after the taggle

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -7,12 +7,12 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
   quotes: Quote[] = [
-      new Quote(1, 'There is nothing permanent except change','Heraclitus'),
-      new Quote(2, 'Let us sacrifice ourselfs today so that our children can have a better tomorrow','A.P.J.Abdul Kalam'),
-      new Quote(3, 'There is no charm equal to tenderness of heart','Jane Austen'),
-      new Quote(4, 'All that we see or seem is but a dream within a dream','Edgar Allan Poe'),
-      new Quote(5, 'The only journey is the one within','Rainer Maria Rilke'),
-      new Quote(6, 'Think in the morning, act in the noon, eat in the evening and sleep in the night','Will Rogers'),
+      new Quote(1, 'There is nothing permanent except change','Heraclitus', 'Joselyne', new Date(2019,8,23)),
+      new Quote(2, 'Let us sacrifice ourselfs today so that our children can have a better tomorrow','A.P.J.Abdul Kalam', 'Jojo', new Date(2019,8,23)),
+      new Quote(3, 'There is no charm equal to tenderness of heart','Jane Austen','Fabiola', new Date(2019,8,23)),
+      new Quote(4, 'All that we see or seem is but a dream within a dream','Edgar Allan Poe', 'Arielle', new Date(2019,8,23)),
+      new Quote(5, 'The only journey is the one within','Rainer Maria Rilke','Ritha', new Date(2019,8,23)),
+      new Quote(6, 'Think in the morning, act in the noon, eat in the evening and sleep in the night','Will Rogers','Gaby', new Date(2019,8,23)),
     ];
 
     toggleDetails(index){
