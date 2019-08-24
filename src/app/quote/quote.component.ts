0bc673575c9a75dbf2ae95dd91477193c96c2369
Joselyne97 <joselynejojo740@gmail.com>
2019-08-24 16:01:44 +0200

    details of how to add quote

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -11,8 +11,7 @@ export class QuoteComponent implements OnInit {
       new Quote(2, 'Let us sacrifice ourselfs today so that our children can have a better tomorrow','A.P.J.Abdul Kalam', 'Jojo', new Date(2019,8,20)),
       new Quote(3, 'There is no charm equal to tenderness of heart','Jane Austen','Fabiola', new Date(2019,8,20)),
       new Quote(4, 'All that we see or seem is but a dream within a dream','Edgar Allan Poe', 'Arielle', new Date(2019,8,23)),
-      new Quote(5, 'The only journey is the one within','Rainer Maria Rilke','Ritha', new Date(2019,8,23)),
-      new Quote(6, 'Think in the morning, act in the noon, eat in the evening and sleep in the night','Will Rogers','Gaby', new Date(2019,8,23)),
+      new Quote(5, 'Think in the morning, act in the noon, eat in the evening and sleep in the night','Will Rogers','Gaby', new Date(2019,8,23)),
     ];
 
     toggleDetails(index){
@@ -27,6 +26,12 @@ export class QuoteComponent implements OnInit {
         this.quotes.splice(index,1);
       }
     }
+  }
+  addNewQuote(quote){
+    let quoteLength = this.quotes.length;
+    quote.id = quoteLength+1;
+    quote.completeDate = new Date(quote.completeDate)
+    this.quotes.push(quote)
   }
   constructor() { }
 
